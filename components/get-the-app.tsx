7a48ab9e6db7b9a1a3b3d332eecd45b6bcdcb177
Maksim Zemlyanikin <zemlyanikin.maksim@gmail.com>
2024-01-29 18:08:03 +0700

    Change get the app buttons min width

--- components/get-the-app.tsx
+++ components/get-the-app.tsx
@@ -6,104 +6,105 @@ export default function GetTheApp() {
         <div className="py-12 md:py-20 border-t border-gray-800">
 
           {/* Section header */}
-          <div className="max-w-5xl mx-auto text-center">
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h2 mb-4 pb-16" data-aos="fade-up">
               Start using it right now!
             </h1>
 
-            {/* Items */}
-            <div className="grid grid-cols-3 gap-8 lg:gap-16 items-top-center lg:max-w-none" data-aos-id-blocks2>
+          </div>
 
-              {/* 1st item */}
-              <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks2]">
-                <svg className="w-36 h-28" width="38" height="24" viewBox="0 0 38 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <g clip-path="url(#clip0_2458_11)">
-                    <path d="M11 2.5H9L8.96342 2.5C8.05169 2.49999 7.31681 2.49997 6.73883 2.57768C6.13876 2.65836 5.63351 2.83096 5.23223 3.23223C4.83096 3.63351 4.65836 4.13876 4.57768 4.73883C4.49997 5.31681 4.49999 6.05169 4.5 6.96342L4.5 7V17L4.5 17.0366C4.49999 17.9483 4.49997 18.6832 4.57768 19.2612C4.65836 19.8612 4.83096 20.3665 5.23223 20.7678C5.63351 21.169 6.13876 21.3416 6.73883 21.4223C7.31681 21.5 8.05168 21.5 8.9634 21.5H9H11H11.0366C11.9483 21.5 12.6832 21.5 13.2612 21.4223C13.8612 21.3416 14.3665 21.169 14.7678 20.7678C15.169 20.3665 15.3416 19.8612 15.4223 19.2612C15.5 18.6832 15.5 17.9483 15.5 17.0366V17V7V6.96343C15.5 6.0517 15.5 5.31681 15.4223 4.73883C15.3416 4.13876 15.169 3.63351 14.7678 3.23223C14.3665 2.83096 13.8612 2.65836 13.2612 2.57768C12.6832 2.49997 11.9483 2.49999 11.0366 2.5L11 2.5Z" stroke="white" />
-                    <path d="M5 16H15" stroke="white" />
-                    <rect x="18.5" y="2.5" width="15" height="19" rx="2.5" stroke="white" />
-                    <path d="M29 18H23" stroke="white" />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_2458_11">
-                      <rect width="38" height="24" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
+          {/* Items */}
+          <div className="grid lg:grid-cols-3 md:grid-cols-3 gap-8 lg:gap-16 items-top-center lg:max-w-none" data-aos-id-blocks2>
 
-                <h4 className="h4 pb-6">Mobile</h4>
+            {/* 1st item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks2]">
+              <svg className="w-36 h-28" width="38" height="24" viewBox="0 0 38 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_2458_11)">
+                  <path d="M11 2.5H9L8.96342 2.5C8.05169 2.49999 7.31681 2.49997 6.73883 2.57768C6.13876 2.65836 5.63351 2.83096 5.23223 3.23223C4.83096 3.63351 4.65836 4.13876 4.57768 4.73883C4.49997 5.31681 4.49999 6.05169 4.5 6.96342L4.5 7V17L4.5 17.0366C4.49999 17.9483 4.49997 18.6832 4.57768 19.2612C4.65836 19.8612 4.83096 20.3665 5.23223 20.7678C5.63351 21.169 6.13876 21.3416 6.73883 21.4223C7.31681 21.5 8.05168 21.5 8.9634 21.5H9H11H11.0366C11.9483 21.5 12.6832 21.5 13.2612 21.4223C13.8612 21.3416 14.3665 21.169 14.7678 20.7678C15.169 20.3665 15.3416 19.8612 15.4223 19.2612C15.5 18.6832 15.5 17.9483 15.5 17.0366V17V7V6.96343C15.5 6.0517 15.5 5.31681 15.4223 4.73883C15.3416 4.13876 15.169 3.63351 14.7678 3.23223C14.3665 2.83096 13.8612 2.65836 13.2612 2.57768C12.6832 2.49997 11.9483 2.49999 11.0366 2.5L11 2.5Z" stroke="white" />
+                  <path d="M5 16H15" stroke="white" />
+                  <rect x="18.5" y="2.5" width="15" height="19" rx="2.5" stroke="white" />
+                  <path d="M29 18H23" stroke="white" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_2458_11">
+                    <rect width="38" height="24" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
 
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-48"
-                  href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
-                  <div className="flex flex-row pr-1 content-center">
-                    <svg className="w-5 h-5 flex" xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256" viewBox="0 0 256 256">
-                      <g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
-                        <path fill="white" d="M 71.811 0.006 H 18.178 C 8.133 0.006 0 8.138 0 18.183 v 53.645 c 0 10.034 8.133 18.167 18.178 18.167 h 53.645 C 81.856 89.994 90 81.862 90 71.817 V 18.183 C 89.989 8.138 81.856 0.006 71.811 0.006 z M 24.882 71.254 c -1.125 1.969 -3.633 2.632 -5.602 1.507 c -1.969 -1.125 -2.632 -3.633 -1.507 -5.602 l 2.925 -5.062 c 3.307 -1.024 5.996 -0.236 8.122 2.328 L 24.882 71.254 z M 53.318 60.208 H 16.086 c -2.272 0 -4.094 -1.822 -4.094 -4.095 c 0 -2.272 1.822 -4.094 4.094 -4.094 h 10.439 l 13.363 -23.161 l -4.173 -7.244 c -1.125 -1.969 -0.461 -4.454 1.507 -5.602 c 1.969 -1.125 4.454 -0.461 5.602 1.507 l 1.788 3.161 l 1.822 -3.15 c 1.125 -1.969 3.633 -2.632 5.602 -1.507 c 1.969 1.125 2.632 3.633 1.507 5.602 L 35.984 52.019 h 12.7 C 52.801 52.019 55.107 56.856 53.318 60.208 z M 73.375 60.231 h -5.917 l 3.993 6.929 c 1.125 1.969 0.461 4.454 -1.507 5.602 c -1.969 1.125 -4.454 0.461 -5.602 -1.507 C 57.615 59.589 52.565 50.86 49.213 45.045 c -3.431 -5.917 -0.979 -11.856 1.44 -13.87 c 2.688 4.612 6.704 11.575 12.07 20.866 h 10.652 c 2.272 0 4.095 1.822 4.095 4.095 C 77.469 58.408 75.647 60.231 73.375 60.231 z" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                      </g>
-                    </svg>
-                    <div className="flex ml-2">
-                      App Store
-                    </div>
+              <h4 className="h4 pb-6">Mobile</h4>
+
+              <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-64"
+                href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
+                <div className="flex flex-row pr-1 content-center">
+                  <svg className="w-5 h-5 flex" xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256" viewBox="0 0 256 256">
+                    <g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+                      <path fill="white" d="M 71.811 0.006 H 18.178 C 8.133 0.006 0 8.138 0 18.183 v 53.645 c 0 10.034 8.133 18.167 18.178 18.167 h 53.645 C 81.856 89.994 90 81.862 90 71.817 V 18.183 C 89.989 8.138 81.856 0.006 71.811 0.006 z M 24.882 71.254 c -1.125 1.969 -3.633 2.632 -5.602 1.507 c -1.969 -1.125 -2.632 -3.633 -1.507 -5.602 l 2.925 -5.062 c 3.307 -1.024 5.996 -0.236 8.122 2.328 L 24.882 71.254 z M 53.318 60.208 H 16.086 c -2.272 0 -4.094 -1.822 -4.094 -4.095 c 0 -2.272 1.822 -4.094 4.094 -4.094 h 10.439 l 13.363 -23.161 l -4.173 -7.244 c -1.125 -1.969 -0.461 -4.454 1.507 -5.602 c 1.969 -1.125 4.454 -0.461 5.602 1.507 l 1.788 3.161 l 1.822 -3.15 c 1.125 -1.969 3.633 -2.632 5.602 -1.507 c 1.969 1.125 2.632 3.633 1.507 5.602 L 35.984 52.019 h 12.7 C 52.801 52.019 55.107 56.856 53.318 60.208 z M 73.375 60.231 h -5.917 l 3.993 6.929 c 1.125 1.969 0.461 4.454 -1.507 5.602 c -1.969 1.125 -4.454 0.461 -5.602 -1.507 C 57.615 59.589 52.565 50.86 49.213 45.045 c -3.431 -5.917 -0.979 -11.856 1.44 -13.87 c 2.688 4.612 6.704 11.575 12.07 20.866 h 10.652 c 2.272 0 4.095 1.822 4.095 4.095 C 77.469 58.408 75.647 60.231 73.375 60.231 z" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                    </g>
+                  </svg>
+                  <div className="flex ml-2">
+                    App Store
                   </div>
-                </a>
+                </div>
+              </a>
 
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-48"
-                  href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
-                  <div className="flex flex-row pr-1 content-center">
-                    <svg className="w-5 h-5 flex" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 256 256">
-                      <path fill="white" d="M48.7,24.9c25.1,25.4,65.3,66.3,87,88.7l12.5,12.8l18.4-18.3c10.2-10.2,18.4-18.7,18.4-19c0-1-156.8-88.5-158.7-88.5C25.3,0.5,34,9.9,48.7,24.9z" />
-                      <path fill="white" d="M13.8,4.3c-0.9,1-2.1,3.1-2.7,4.6C10.3,10.9,10,47.8,10,128c0,127.3-0.3,120.7,6,125.2l3.2,2.3L81,193.7l61.8-61.8l-15.9-16.1c-8.8-8.9-37.4-37.9-63.7-64.7C18.2,5.3,15.3,2.6,13.8,4.3z" />
-                      <path fill="white" d="M174,112.8l-19.6,19.6l16.6,16.8c9.2,9.3,17.1,17,17.6,17.2c1.2,0.3,51.3-27.2,53.9-29.6c1-0.9,2.3-3.1,2.9-5c1.7-5.1-0.9-9.7-7.9-13.9c-5-3-36.7-20.8-42-23.5C193.8,93.3,191.5,95.2,174,112.8z" />
-                      <path fill="white" d="M92.3,194.3c-30.9,30.7-55.9,55.9-55.4,55.9c0.3,0,2.9-1.3,5.7-2.9c5.5-3,114.8-63,127.4-69.8c13.2-7,13.7-4.5-4.5-22.8c-8.9-8.9-16.3-16.1-16.6-16.1C148.6,138.5,123.1,163.6,92.3,194.3z" />
-                    </svg>
-                    <div className="flex ml-2">
-                      Google Play
-                    </div>
+              <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-64"
+                href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
+                <div className="flex flex-row pr-1 content-center">
+                  <svg className="w-5 h-5 flex" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 256 256">
+                    <path fill="white" d="M48.7,24.9c25.1,25.4,65.3,66.3,87,88.7l12.5,12.8l18.4-18.3c10.2-10.2,18.4-18.7,18.4-19c0-1-156.8-88.5-158.7-88.5C25.3,0.5,34,9.9,48.7,24.9z" />
+                    <path fill="white" d="M13.8,4.3c-0.9,1-2.1,3.1-2.7,4.6C10.3,10.9,10,47.8,10,128c0,127.3-0.3,120.7,6,125.2l3.2,2.3L81,193.7l61.8-61.8l-15.9-16.1c-8.8-8.9-37.4-37.9-63.7-64.7C18.2,5.3,15.3,2.6,13.8,4.3z" />
+                    <path fill="white" d="M174,112.8l-19.6,19.6l16.6,16.8c9.2,9.3,17.1,17,17.6,17.2c1.2,0.3,51.3-27.2,53.9-29.6c1-0.9,2.3-3.1,2.9-5c1.7-5.1-0.9-9.7-7.9-13.9c-5-3-36.7-20.8-42-23.5C193.8,93.3,191.5,95.2,174,112.8z" />
+                    <path fill="white" d="M92.3,194.3c-30.9,30.7-55.9,55.9-55.4,55.9c0.3,0,2.9-1.3,5.7-2.9c5.5-3,114.8-63,127.4-69.8c13.2-7,13.7-4.5-4.5-22.8c-8.9-8.9-16.3-16.1-16.6-16.1C148.6,138.5,123.1,163.6,92.3,194.3z" />
+                  </svg>
+                  <div className="flex ml-2">
+                    Google Play
                   </div>
-                </a>
-              </div>
-
-              {/* 2nd item */}
-              <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks2]">
-                <svg className="w-28 h-28" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M3.5 8C3.5 7.04306 3.50106 6.37565 3.56876 5.87208C3.63453 5.3829 3.75483 5.12385 3.93934 4.93934C4.12385 4.75483 4.3829 4.63453 4.87208 4.56876C5.37565 4.50106 6.04306 4.5 7 4.5H17C17.9569 4.5 18.6244 4.50106 19.1279 4.56876C19.6171 4.63453 19.8762 4.75483 20.0607 4.93934C20.2452 5.12385 20.3655 5.3829 20.4312 5.87208C20.4989 6.37565 20.5 7.04306 20.5 8V16.5H3.5V8Z" stroke="white" />
-                  <path d="M3.66667 16.5C3.02233 16.5 2.5 17.0223 2.5 17.6667C2.5 18.6792 3.32081 19.5 4.33333 19.5H19.6667C20.6792 19.5 21.5 18.6792 21.5 17.6667C21.5 17.0223 20.9777 16.5 20.3333 16.5H3.66667Z" stroke="white" />
-                </svg>
-                <h4 className="h4 pb-6">Desktop</h4>
-
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-48"
-                  href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
-                  <div className="flex flex-row pr-1 content-center">
-                    <svg className="w-5 h-5 flex" xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256" viewBox="0 0 256 256">
-                      <g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
-                        <path fill="white" d="M 71.811 0.006 H 18.178 C 8.133 0.006 0 8.138 0 18.183 v 53.645 c 0 10.034 8.133 18.167 18.178 18.167 h 53.645 C 81.856 89.994 90 81.862 90 71.817 V 18.183 C 89.989 8.138 81.856 0.006 71.811 0.006 z M 24.882 71.254 c -1.125 1.969 -3.633 2.632 -5.602 1.507 c -1.969 -1.125 -2.632 -3.633 -1.507 -5.602 l 2.925 -5.062 c 3.307 -1.024 5.996 -0.236 8.122 2.328 L 24.882 71.254 z M 53.318 60.208 H 16.086 c -2.272 0 -4.094 -1.822 -4.094 -4.095 c 0 -2.272 1.822 -4.094 4.094 -4.094 h 10.439 l 13.363 -23.161 l -4.173 -7.244 c -1.125 -1.969 -0.461 -4.454 1.507 -5.602 c 1.969 -1.125 4.454 -0.461 5.602 1.507 l 1.788 3.161 l 1.822 -3.15 c 1.125 -1.969 3.633 -2.632 5.602 -1.507 c 1.969 1.125 2.632 3.633 1.507 5.602 L 35.984 52.019 h 12.7 C 52.801 52.019 55.107 56.856 53.318 60.208 z M 73.375 60.231 h -5.917 l 3.993 6.929 c 1.125 1.969 0.461 4.454 -1.507 5.602 c -1.969 1.125 -4.454 0.461 -5.602 -1.507 C 57.615 59.589 52.565 50.86 49.213 45.045 c -3.431 -5.917 -0.979 -11.856 1.44 -13.87 c 2.688 4.612 6.704 11.575 12.07 20.866 h 10.652 c 2.272 0 4.095 1.822 4.095 4.095 C 77.469 58.408 75.647 60.231 73.375 60.231 z" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                      </g>
-                    </svg>
-                    <div className="flex ml-2">
-                      App Store
-                    </div>
-                  </div>
-                </a>
-              </div>
-
-              {/* 3d item */}
-              <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks2]">
-                <svg className="w-28 h-28" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="12" cy="12" r="7.5" stroke="white" />
-                  <path d="M14.5 12C14.5 14.1651 14.1701 16.1029 13.6532 17.4813C13.394 18.1723 13.0975 18.6969 12.7936 19.0396C12.4892 19.383 12.2199 19.5 12 19.5C11.7801 19.5 11.5108 19.383 11.2064 19.0396C10.9025 18.6969 10.606 18.1723 10.3468 17.4813C9.82994 16.1029 9.5 14.1651 9.5 12C9.5 9.83494 9.82994 7.89713 10.3468 6.51871C10.606 5.82765 10.9025 5.30314 11.2064 4.96038C11.5108 4.61704 11.7801 4.5 12 4.5C12.2199 4.5 12.4892 4.61704 12.7936 4.96038C13.0975 5.30314 13.394 5.82765 13.6532 6.51871C14.1701 7.89713 14.5 9.83494 14.5 12Z" stroke="white" />
-                  <path d="M4.5 12H19.5" stroke="white" stroke-linecap="round" />
-                </svg>
-
-                <h4 className="h4 pb-6">Web</h4>
-
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-48"
-                  href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
-                  <div className="flex flex-row pr-1 content-center">
-                    Try right in your browser
-                  </div>
-                </a>
-              </div>
-
+                </div>
+              </a>
             </div>
+
+            {/* 2nd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks2]">
+              <svg className="w-28 h-28" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.5 8C3.5 7.04306 3.50106 6.37565 3.56876 5.87208C3.63453 5.3829 3.75483 5.12385 3.93934 4.93934C4.12385 4.75483 4.3829 4.63453 4.87208 4.56876C5.37565 4.50106 6.04306 4.5 7 4.5H17C17.9569 4.5 18.6244 4.50106 19.1279 4.56876C19.6171 4.63453 19.8762 4.75483 20.0607 4.93934C20.2452 5.12385 20.3655 5.3829 20.4312 5.87208C20.4989 6.37565 20.5 7.04306 20.5 8V16.5H3.5V8Z" stroke="white" />
+                <path d="M3.66667 16.5C3.02233 16.5 2.5 17.0223 2.5 17.6667C2.5 18.6792 3.32081 19.5 4.33333 19.5H19.6667C20.6792 19.5 21.5 18.6792 21.5 17.6667C21.5 17.0223 20.9777 16.5 20.3333 16.5H3.66667Z" stroke="white" />
+              </svg>
+              <h4 className="h4 pb-6">Desktop</h4>
+
+              <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-64"
+                href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
+                <div className="flex flex-row pr-1 content-center">
+                  <svg className="w-5 h-5 flex" xmlns="http://www.w3.org/2000/svg" version="1.1" width="256" height="256" viewBox="0 0 256 256">
+                    <g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+                      <path fill="white" d="M 71.811 0.006 H 18.178 C 8.133 0.006 0 8.138 0 18.183 v 53.645 c 0 10.034 8.133 18.167 18.178 18.167 h 53.645 C 81.856 89.994 90 81.862 90 71.817 V 18.183 C 89.989 8.138 81.856 0.006 71.811 0.006 z M 24.882 71.254 c -1.125 1.969 -3.633 2.632 -5.602 1.507 c -1.969 -1.125 -2.632 -3.633 -1.507 -5.602 l 2.925 -5.062 c 3.307 -1.024 5.996 -0.236 8.122 2.328 L 24.882 71.254 z M 53.318 60.208 H 16.086 c -2.272 0 -4.094 -1.822 -4.094 -4.095 c 0 -2.272 1.822 -4.094 4.094 -4.094 h 10.439 l 13.363 -23.161 l -4.173 -7.244 c -1.125 -1.969 -0.461 -4.454 1.507 -5.602 c 1.969 -1.125 4.454 -0.461 5.602 1.507 l 1.788 3.161 l 1.822 -3.15 c 1.125 -1.969 3.633 -2.632 5.602 -1.507 c 1.969 1.125 2.632 3.633 1.507 5.602 L 35.984 52.019 h 12.7 C 52.801 52.019 55.107 56.856 53.318 60.208 z M 73.375 60.231 h -5.917 l 3.993 6.929 c 1.125 1.969 0.461 4.454 -1.507 5.602 c -1.969 1.125 -4.454 0.461 -5.602 -1.507 C 57.615 59.589 52.565 50.86 49.213 45.045 c -3.431 -5.917 -0.979 -11.856 1.44 -13.87 c 2.688 4.612 6.704 11.575 12.07 20.866 h 10.652 c 2.272 0 4.095 1.822 4.095 4.095 C 77.469 58.408 75.647 60.231 73.375 60.231 z" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                    </g>
+                  </svg>
+                  <div className="flex ml-2">
+                    Mac App Store
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            {/* 3d item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks2]">
+              <svg className="w-28 h-28" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="12" r="7.5" stroke="white" />
+                <path d="M14.5 12C14.5 14.1651 14.1701 16.1029 13.6532 17.4813C13.394 18.1723 13.0975 18.6969 12.7936 19.0396C12.4892 19.383 12.2199 19.5 12 19.5C11.7801 19.5 11.5108 19.383 11.2064 19.0396C10.9025 18.6969 10.606 18.1723 10.3468 17.4813C9.82994 16.1029 9.5 14.1651 9.5 12C9.5 9.83494 9.82994 7.89713 10.3468 6.51871C10.606 5.82765 10.9025 5.30314 11.2064 4.96038C11.5108 4.61704 11.7801 4.5 12 4.5C12.2199 4.5 12.4892 4.61704 12.7936 4.96038C13.0975 5.30314 13.394 5.82765 13.6532 6.51871C14.1701 7.89713 14.5 9.83494 14.5 12Z" stroke="white" />
+                <path d="M4.5 12H19.5" stroke="white" stroke-linecap="round" />
+              </svg>
+
+              <h4 className="h4 pb-6">Web</h4>
+
+              <a className="btn text-white bg-gray-700 hover:bg-gray-800 rounded-lg mb-4 min-w-64"
+                href="https://apps.apple.com/am/app/teamin-manage-tasks-and-chat/id6473703636">
+                <div className="flex flex-row pr-1 content-center">
+                  Try right in your browser
+                </div>
+              </a>
+            </div>
+
           </div>
         </div>
       </div>
